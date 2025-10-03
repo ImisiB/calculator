332df012479f792e0fd05f83c87a9e4fad05a25f
Imisi Bailey <imisibailey@gmail.com>
2025-10-03 16:15:51 +0100

    Updated calculator

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -2,8 +2,6 @@ let calculation = '';
 
 const oldResult = localStorage.getItem('answers')
 
-console.log(oldResult);
-
 
 document.querySelector('.result')
   .innerHTML = oldResult;
@@ -12,13 +10,75 @@ document.querySelector('.result')
 function answer() {
   document.querySelector('.js-result')
     .innerHTML = calculation;
-}
+  }
 
 
 function calcResult() {
   const result = calculation; 
-
   document.querySelector('.js-result')
     .innerHTML = result;
   localStorage.setItem('answers', JSON.stringify(result))
 }
+
+document.querySelector('body').addEventListener('keydown', (event) => {
+  if(event.key === '1') {
+    calculation += '1';
+    answer();
+  } else if(event.key === '2') {
+    calculation += '2';
+    answer();
+  } else if(event.key === '3') {
+    calculation += '3';
+    answer();
+  } else if(event.key === '4') {
+    calculation += '4';
+    answer();
+  } else if(event.key === '5') {
+    calculation += '5';
+    answer();
+  } else if(event.key === '6') {
+    calculation += '6';
+    answer();
+  } else if(event.key === '7') {
+    calculation += '7';
+    answer();
+  } else if(event.key === '8') {
+    calculation += '8';
+    answer();
+  } else if(event.key === '9') {
+    calculation += '9';
+    answer();
+  } else if(event.key === '+') {
+    calculation += ' + ';
+    answer();
+  } else if(event.key === '-') {
+    calculation += ' - ';
+    answer();
+  } else if(event.key === '/') {
+    calculation += ' / ';
+    answer();
+  } else if(event.key === '*') {
+    calculation += ' * ';
+    answer();
+  } else if(event.key === '.') {
+    calculation += '.';
+    answer();
+  } else if(event.key === '>') {
+    calculation += ' > ';
+    answer();
+  } else if(event.key === '<') {
+    calculation += ' < ';
+    answer();
+  } else if(event.key === '=') {
+    calculation += ' === ';
+    answer();
+  } else if(event.key === 'Backspace') {
+    calculation = '';
+    answer();
+    localStorage.setItem('answers', '')
+  } else if(event.key === 'Enter') {
+    calculation = eval(calculation);
+    calcResult();
+    answer();
+  }
+})
